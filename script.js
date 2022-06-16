7101d5d9833b64a55ccade12b2c03059198a9e4a
Te <smoochbanner@icloud.com>
2022-06-15 22:06:54 -0700

    updates to js

--- script.js
+++ script.js
@@ -6,28 +6,71 @@ var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"
 
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
+var passwordLength
+
+var generatePassword
+
+var passwordText
+
+// var allChar = array // these may need to move
+
 const array = numbers.concat(specialCharacters, lowerCase, upperCase)
 
 console.log(array);
 
-// var numbers = "0123456789".split("") // split method
-// var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("")
+// Validate password length-- make sure its between 8-128 characters
+  // prompt user for special characters
+  // If password length is greater than or equal to 8 and password length is greater than or equal to 128
 
-// var characters = "0123457689abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+[];'./{}:>?|"
-// prompt user for password length
-  //Validate password length-- make sure its between 8-128 characters
-  //prompt user for special characters
+
+// prompt user for password length  //prompt code sourced from javapoint.com
+var passwordLength = true
+function passwordLength () {
+var a = prompt("Enter Password Length")
+  
+}
 
 function generatePassword() {
+ var hello = "Enter Password Length";
+  var input = prompt (hello, "8-128 characters");
+  if (i => 8) {
+    prompt ("Password must be atleast 8 Characters")
+  } else if (i ==< 8){
+    prompt ("Apply Special Characters")
+  }
+//  let input = { 
+//   newPassword
+
+  
+
+  }
+
+//  confirm(input);
+
+// if(!variableA && variableB ) {
+//   //end the function early
+//   //alert the user they had bad data
+//   return"";
+// }
+  
 
   
   
-  }
+  
+//declare a new empty string to store the password characters
+
+for (i = 0; i < passwordLength; i++)
+
+//randomly select chars
+//adding that chat to building password string
+
+//return completed string;
 
 
 // Assignment Code
 
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"); 
+
 
 // Write password to the #password input
 function writePassword() {
